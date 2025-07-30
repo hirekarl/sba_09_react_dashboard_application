61416262c3f68f3e285018247b06b209cb8aacc0
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-30 18:05:09 -0400

    Removed card Bootstrap component from statistics.

--- task-dashboard/src/components/TaskStatistics/TaskStatistics.tsx
+++ task-dashboard/src/components/TaskStatistics/TaskStatistics.tsx
@@ -31,34 +31,24 @@ export default function TaskStatistics({ tasks }: TaskStatisticsProps) {
 
   return (
     <div className="mb-3 border rounded-3 p-3">
-      <h2 className="fs-4 text-center">Statistics</h2>
+      <h2 className="fs-4 text-center">Task Statistics</h2>
       <h3 className="fs-5 text-center mb-3">Total Tasks: {totalTasksCount}</h3>
       <div className="d-flex justify-content-between gap-3">
-        <div className="card w-100">
-          <div className="card-body">
-            <div className="card-title">
-              <h3 className="fs-6 text-center">Total By Status</h3>
-            </div>
-            <div className="card-text">
-              <ul>
-                <li>Pending: {pendingTasksCount}</li>
-                <li>In Progress: {inProgressTasksCount}</li>
-                <li>Completed: {completedTasksCount}</li>
-              </ul>
-            </div>
-          </div>
+        <div className="border rounded-3 p-3 w-100">
+          <h3 className="fs-6 text-center">Total By Status</h3>
+          <ul>
+            <li>Pending: {pendingTasksCount}</li>
+            <li>In Progress: {inProgressTasksCount}</li>
+            <li>Completed: {completedTasksCount}</li>
+          </ul>
         </div>
-        <div className="card w-100">
-          <div className="card-body">
-            <div className="card-title">
-              <h3 className="fs-6 text-center">Total By Priority</h3>
-              <ul>
-                <li>High: {highPriorityTasksCount}</li>
-                <li>Medium: {mediumPriorityTasksCount}</li>
-                <li>Low: {lowPriorityTasksCount}</li>
-              </ul>
-            </div>
-          </div>
+        <div className="border rounded-3 p-3 w-100">
+          <h3 className="fs-6 text-center">Total By Priority</h3>
+          <ul>
+            <li>High: {highPriorityTasksCount}</li>
+            <li>Medium: {mediumPriorityTasksCount}</li>
+            <li>Low: {lowPriorityTasksCount}</li>
+          </ul>
         </div>
       </div>
     </div>
