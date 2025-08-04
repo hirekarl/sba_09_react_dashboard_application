fa1a22ce422ae918d64fda987c6b87b23e2fd06b
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-04 10:54:29 -0400

    Made import and export buttons small.

--- task-dashboard/src/components/ImportExport/ImportExport.tsx
+++ task-dashboard/src/components/ImportExport/ImportExport.tsx
@@ -3,8 +3,8 @@ import type { ImportExportProps } from "../../types";
 export default function ImportExport({onImport, exportURL}: ImportExportProps) {
   return (
     <div className="btn-group w-100 mb-3">
-      <button type="button" className="btn btn-primary" onClick={onImport} disabled>Import Tasks</button>
-      <a href={exportURL} download="tasks.json" type="button" className="btn btn-primary" >Export Tasks</a>
+      <button type="button" className="btn btn-sm btn-primary" onClick={onImport} disabled>Import Tasks</button>
+      <a href={exportURL} download="tasks.json" type="button" className="btn btn-sm btn-primary" >Export Tasks</a>
     </div>
   )
 }
