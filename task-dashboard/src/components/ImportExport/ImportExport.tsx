8f5e0292afeb82f696774e276c7948f1273e60e6
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-05 11:58:11 -0400

    Fixed typo.

--- task-dashboard/src/components/ImportExport/ImportExport.tsx
+++ task-dashboard/src/components/ImportExport/ImportExport.tsx
@@ -4,7 +4,7 @@ export default function ImportExport({onImport, exportURL}: ImportExportProps) {
   return (
     <div className="btn-group w-100 mb-3">
       <button type="button" className="btn btn-sm btn-primary" onClick={onImport} disabled>Import Tasks</button>
-      <a href={exportURL} download="tasks.json" type="button" className="btn btn-sm btn-primary" >Export Tasks</a>
+      <a href={exportURL} download="tasks.json" type="button" className="btn btn-sm btn-primary">Export Tasks</a>
     </div>
   )
 }
